a2713a6438ace42b273c11c21436003d028dc0cc
KMcCrear <64017644+KMcCrear@users.noreply.github.com>
2022-05-18 16:40:53 +0100

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## ETL Multithreading Approach
+
+### Current Performance
+Script takes approx 18 minutes to generate 2 million records and insert them into the target DB using SQL*Loader
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import cx_Oracle
-
 import db_DAO
-import dbconfig
 import data_generate
 from time import perf_counter
-import time
 from threading import Thread
-import logging
-
 
 
 if __name__ == '__main__':
-    """DB stuff"""
+
     start = perf_counter()
     data_generate.generate_csv_data('test_data.csv', 1000000)
     data_generate.generate_csv_data('employee_data.csv', 1000000)
